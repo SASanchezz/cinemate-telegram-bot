c9e4923ebb6923d388b33e61160c11bbb15415bb
Lizzzka <93037236+lizzzuhaaa@users.noreply.github.com>
2024-04-01 16:59:29 +0300

    Inizializing commands
    Inizializing basic menu of available commands

--- bot.py
+++ bot.py
@@ -5,10 +5,24 @@ from aiogram.filters.command import Command
 
 from Configuration.bot_config import bot
 
-
 # logging and dispatcher
 logging.basicConfig(level=logging.INFO)
 dp = Dispatcher()
+
+
+# list of available commands in bot
+async def set_up_commands():
+    bot_commands = [
+        types.BotCommand(command="/start", description="Start Cinemate"),
+        types.BotCommand(command="/help", description="Help"),
+        types.BotCommand(command="/name_search", description="Search by name"),
+        types.BotCommand(command="/filter_search", description="Search by criteria"),
+        types.BotCommand(command="/recommend", description="Get recommendation"),
+        types.BotCommand(command="/my_movielist", description="My movielist"),
+        types.BotCommand(command="/info", description="Get information for the current film")
+
+    ]
+    await bot.set_my_commands(bot_commands)
 
 
 # /start
@@ -19,6 +33,7 @@ async def cmd_start(message: types.Message):
 
 # start polling
 async def main():
+    await set_up_commands()
     await dp.start_polling(bot)
 
 
