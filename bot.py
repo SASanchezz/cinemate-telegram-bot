3f5d47a9d8ef60343270dae4423709cb06277684
Lizzzka <93037236+lizzzuhaaa@users.noreply.github.com>
2024-04-03 21:58:54 +0300

    Set up list of commands in bot

--- bot.py
+++ bot.py
@@ -4,7 +4,21 @@ from aiogram import Dispatcher
 from aiogram.fsm.storage.memory import MemoryStorage
 
 from Configuration.bot_config import bot
-from handlers import router
+from handlers import router, types
+
+
+# list of available commands in bot
+async def set_up_commands():
+    bot_commands = [
+        types.BotCommand(command="/start", description="Start Cinemate"),
+        types.BotCommand(command="/help", description="Help"),
+        types.BotCommand(command="/name_search", description="Search by name"),
+        types.BotCommand(command="/filter_search", description="Search by criteria"),
+        types.BotCommand(command="/recommend", description="Get recommendation"),
+        types.BotCommand(command="/my_movielist", description="My movielist"),
+        types.BotCommand(command="/info", description="Get information for the current film")
+    ]
+    await bot.set_my_commands(bot_commands)
 
 
 async def main():
